dbf664e52d02798e9068f9dc4bbedf662e8a541b
Jorge Pablos-Araque <jpablos11@gmail.com>
2023-05-15 11:27:50 +0100

    fixed issue with outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "parameter_names" {
   description = "The names of the created SSM parameters"
-  value       = aws_ssm_parameter.parameter[*].name
+  value       = [for k, v in aws_ssm_parameter.parameter : v.name]
+}
+
+output "parameter_arns" {
+  description = "The names of the created SSM parameters"
+  value       = [for k, v in aws_ssm_parameter.parameter : v.arn]
 }
